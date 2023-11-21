87dc98d4cff46576a8cee38a95ff3c5da86b8d0a
Alex B <aleblakl@gmail.com>
2023-11-20 23:54:01 -0500

    Update

--- consume_api.py
+++ consume_api.py
@@ -1,7 +1,53 @@
-import requests
-import json
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
-response = requests.get('http://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+app = Flask(__name__)
 
-for data in response.json()['items']:
-    print(data['title'])
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    publisher = db.Column(db.String(100), nullable=False)
+
+@app.route('/books', methods=['GET'])
+def get_all_books():
+    books = Book.query.all()
+    return jsonify([{'id': book.id, 'book_name': book.book_name, 'author': book.author, 'publisher': book.publisher} for book in books])
+
+@app.route('/books/<int:id>', methods=['GET'])
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return jsonify({'id': book.id, 'book_name': book.book_name, 'author': book.author, 'publisher': book.publisher})
+
+@app.route('/books', methods=['POST'])
+def create_book():
+    data = request.json
+    new_book = Book(book_name=data['book_name'], author=data['author'], publisher=data['publisher'])
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify({'message': 'Book created successfully', 'id': new_book.id})
+
+@app.route('/books/<int:id>', methods=['PUT'])
+def update_book(id):
+    book = Book.query.get_or_404(id)
+    data = request.json
+    book.book_name = data.get('book_name', book.book_name)
+    book.author = data.get('author', book.author)
+    book.publisher = data.get('publisher', book.publisher)
+    db.session.commit()
+    return jsonify({'message': 'Book updated successfully'})
+
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    book = Book.query.get_or_404(id)
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify({'message': 'Book deleted successfully'})
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
