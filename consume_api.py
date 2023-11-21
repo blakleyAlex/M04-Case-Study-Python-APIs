82d5940bfefc189fe091b090a5de30da28beb19f
Alex B <aleblakl@gmail.com>
2023-11-20 23:51:39 -0500

    Initial commit

---
+++ consume_api.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+response = requests.get('http://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+
+for data in response.json()['items']:
+    print(data['title'])
